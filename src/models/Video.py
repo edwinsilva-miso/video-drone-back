412d28aa39a0b9138c07a020e75f7482b873e746
Santiago Alarcon <s.alarcong@uniandes.edu.co>
2024-04-13 13:53:57 -0500

    Actualización de estados en videos

--- src/models/Video.py
+++ src/models/Video.py
@@ -1,7 +1,13 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum
 from sqlalchemy.orm import relationship
-
 from src.database.declarative_base import Base
+import enum
+
+
+class StatusVideo(enum.Enum):
+    processed = "processed"
+    uploaded = "uploaded"
 
 
 class Video(Base):
@@ -10,6 +16,7 @@ class Video(Base):
     id = Column(Integer, primary_key=True)
     description = Column(String)
     path = Column(String)
-    user_id = Column(ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    status = Column(sqlalchemy.Enum(StatusVideo))
 
     user = relationship('User', back_populates='videos')
