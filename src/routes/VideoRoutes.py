335bd7ec8bc3710fee7f526d075a135372a362aa
Santiago Alarcon <s.alarcong@uniandes.edu.co>
2024-04-13 01:51:38 -0500

    Logica para recibir videos y procesarlos

--- src/routes/VideoRoutes.py
+++ src/routes/VideoRoutes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request, jsonify
 
 from src.services.AuthenticationService import AuthenticationService
 from src.services.VideoUploadService import VideoUploadService
+import uuid
+import asyncio
 
 main = Blueprint('video_blueprint', __name__)
 
@@ -11,7 +13,20 @@ def upload_video():
     has_access = AuthenticationService.verify_token(request.headers)
 
     if has_access:
-        response = VideoUploadService.upload()
+
+        if 'video' not in request.files:
+            return "No video file provided", 400
+
+        video_file = request.files['video']
+        if video_file.filename == '':
+            return "No selected file", 400
+
+        description = request.values["description"]
+
+        random_name = uuid.uuid4().__str__()
+        response = VideoUploadService.upload(description, random_name, '1')
+
+        asyncio.run(VideoUploadService.save_video(video_file, random_name))  # Use asyncio.run() to execute the coroutine
         return {'status': response}
     else:
         response = jsonify({'message': 'Unauthorized'})
--- src/services/VideoUploadService.py
+++ src/services/VideoUploadService.py
@@ -1,4 +1,9 @@
 from decouple import config
+from src.models.Video import Video
+from src.models.User import User
+from src.database.declarative_base import Session
+import cv2
+
 
 # from src.database.declarative_base import Session
 # from src.models.User import User
@@ -6,13 +11,102 @@ from decouple import config
 
 class VideoUploadService:
     video_path = config('VIDEO_PATH')
+    source_path = config('SOURCE_PATH')
 
     @classmethod
-    def upload(cls):
+    def upload(cls, description, video_name, user_id):
         """
         Implement here the video upload process
         """
+        video_path = config('VIDEO_PATH')
+
         # These lines are a test for query videos for an user
-        # user = Session.query(User).filter_by(id=1).first()
-        # print(user.videos)
-        return 'Video uploaded!'
+        user = Session.query(User).filter_by(id=user_id).first()
+        print(user)
+
+        if description:
+            new_video = Video(
+                description=description,
+                path=video_path + video_name + '-output.mp4',
+                user_id=user.id
+            )
+            Session.add(new_video)
+            Session.commit()
+            return 'Video uploaded!'
+
+        return False
+
+    @classmethod
+    async def save_video(cls, file, filename):
+        video_path = config('VIDEO_PATH')
+        source_path = config('SOURCE_PATH')
+
+        temp_filename = video_path + filename + '.mp4'
+        print(temp_filename)
+        with open(temp_filename, 'wb') as f:
+            while True:
+                chunk = file.read(1024)
+                if not chunk:
+                    break
+                f.write(chunk)
+        print(f"Video saved as {temp_filename}")
+
+        new_name = video_path + filename + '-output.mp4'
+        logo_path = source_path + 'logo.jpeg'
+        print(logo_path)
+        cls.change_aspect_ratio(temp_filename, new_name, logo_path, logo_path, 30)
+
+    @classmethod
+    def change_aspect_ratio(cls, video_path, output_path, start_image_path, end_image_path, num_frames):
+        # Read the video file
+        cap = cv2.VideoCapture(video_path)
+
+        # Get the video's width and height
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        # Calculate the new width and height for a 16:9 aspect ratio
+        new_width = int(height * 16 / 9)
+        new_height = height
+
+        # Get the original frame rate
+        frame_rate = cap.get(cv2.CAP_PROP_FPS)
+
+        # Create a video writer object with the original frame rate
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter(output_path, fourcc, frame_rate, (new_width, new_height))
+
+        # Read start image
+        start_image = cv2.imread(start_image_path)
+        start_image = cv2.resize(start_image, (new_width, new_height))
+
+        # Read end image
+        end_image = cv2.imread(end_image_path)
+        end_image = cv2.resize(end_image, (new_width, new_height))
+
+        # Write start frames
+        for _ in range(num_frames):
+            out.write(start_image)
+
+        # Loop over the frames in the video
+        while True:
+            # Read a frame from the video
+            ret, frame = cap.read()
+
+            # If the frame is empty, break out of the loop
+            if not ret:
+                break
+
+            # Resize the frame to the new aspect ratio
+            resized_frame = cv2.resize(frame, (new_width, new_height))
+
+            # Write the resized frame to the output video
+            out.write(resized_frame)
+
+        # Write end frames
+        for _ in range(num_frames):
+            out.write(end_image)
+
+        # Release the video capture and writer objects
+        cap.release()
+        out.release()
