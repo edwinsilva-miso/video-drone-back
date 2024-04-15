e2b18cd01fd4534c301e6f8dcb2f4f6397d2c491
Miguel Avila <ma.avilat12@uniandes.edu.co>
2024-04-14 21:34:54 -0500

    local fix for docker files

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from src.initialize import init_app
 configuration = configuration['development']
 
 if __name__ == '__main__':
-    init_app(configuration).run(host="0.0.0.0")
+    init_app(configuration).run(host="0.0.0.0", port=8080)
